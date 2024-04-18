6b200c250fd915c2fc79faee83ac4da9e38e70e5
Hüseyin Kurt <kurth2517@gmail.com>
2024-04-18 20:41:40 +0300

    "commit"

--- StoreApp/Controllers/HomeController.cs
+++ StoreApp/Controllers/HomeController.cs
@@ -2,11 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace StoreApp.Controllers
 {
-	public class HomeController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
 }
