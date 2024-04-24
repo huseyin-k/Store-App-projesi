f065d563ef210a5cbaadcd57b9bba5e725ea6994
Hüseyin Kurt <kurth2517@gmail.com>
2024-04-24 15:40:26 +0300

    "commit"

--- StoreApp/obj/Release/net8.0/StoreApp.AssemblyInfo.cs
+++ StoreApp/obj/Release/net8.0/StoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33338ce4dbb46557c52c8597811b4ccb46983a11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+426f779eb48750b31991561d0036de09bb3cb67b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
