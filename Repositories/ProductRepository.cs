b80a12ea4f818e26d16e61e12c4efbbaffbcc284
Hüseyin Kurt <kurth2517@gmail.com>
2024-05-02 16:38:38 +0300

    "starting the service part"

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -15,6 +15,11 @@ namespace Repositories
 		}
 
 		public IQueryable<Product> GetAllProducts(bool trackChanges) => FindAll(trackChanges); 
+
+		public Product? GetOneProduct(int id, bool trackChanges)
+		{
+			return FindByCondition(p=> p.ProductId.Equals(id) ,trackChanges);
+		}
 	
 	}
-}
+} 
